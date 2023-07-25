c64c8ea516f3f0574b76573af7b18fc3183eb76b
Michael Van <michaelvan996@gmail.com>
2023-07-25 07:23:03 -0400

    setted up database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 const cors = require("cors");
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const ap = express();
+
 
 const PORT = process.env.PORT || 8080;
+const mongoDB = "mongodb://127.0.0.1:27017/basic-mern-app";
 
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Connected to the Database');
+    })
+    .catch((err) => console.log(err));
+
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Sever listening on port ${PORT}`);
+});
